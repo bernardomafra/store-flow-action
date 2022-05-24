a8b0b528d1cf4dfc34c892deedf92cb657327baf
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2022-05-23 23:59:57 -0300

    fix: uncomment headless argument

--- src/services/chrome.py
+++ src/services/chrome.py
@@ -59,7 +59,7 @@ class Chrome:
         user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36"
 
         chrome_options.add_argument(f"user-agent={user_agent}")
-        # chrome_options.add_argument("headless")
+        chrome_options.add_argument("headless")
 
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
         self.logger.info("Selenium initialized")
